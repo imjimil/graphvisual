89ef2ff83a8cac2a2c83488c689d4e363534b132
abc <prajapatijimil@yahoo.com>
2023-04-08 19:47:15 -0400

    don't need to refresh page to simulate another graph.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ const App = () => {
   const [showGraph, setShowGraph] = useState(false);
   const [valueOfK, setValueOfK] = useState(2);
   const [algorithm, setAlgorithm] = useState('firstfit');
+  const [key, setKey] = useState(0);
+
 
 
   const handleSubmit = (event) => {
@@ -14,7 +16,8 @@ const App = () => {
     console.log(numVertices)
     // call the Graph component with the number of vertices
     if (numVertices !== null) {
-      console.log("happens")
+      console.log("happens");
+      setKey(key + 1);
       setShowGraph(true);
     }
   };
@@ -24,46 +27,46 @@ const App = () => {
       <nav><h1>Graph Simulation</h1></nav>
       <form onSubmit={handleSubmit}>
         <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-        <label style={{ marginBottom: '10px' }}>
-          Number of vertices:
-          <select
-            value={numVertices}
-            onChange={(event) => setNumVertices(event.target.value)}
-            style={{ marginLeft: '8px' }}
-          >
-            <option value="5" defaultValue>5</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-          </select>
-        </label>
-
-        <label style={{ marginBottom: '10px' }}>
-          Value of k:
-          <select
-            value={valueOfK}
-            onChange={(event) => setValueOfK(event.target.value)}
-            style={{ marginLeft: '8px' }}
-          >
-            <option value="2" defaultValue>2</option>
-            <option value="4">4</option>
-          </select>
-        </label>
+          <label style={{ marginBottom: '10px' }}>
+            Number of vertices:
+            <select
+              value={numVertices}
+              onChange={(event) => setNumVertices(event.target.value)}
+              style={{ marginLeft: '8px' }}
+            >
+              <option value="5" defaultValue>5</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+            </select>
+          </label>
 
           <label style={{ marginBottom: '10px' }}>
-          Algorithm:
-          <select value={algorithm} onChange={(event) => setAlgorithm(event.target.value)} style={{ marginLeft: '15px' }}>
-            <option value="firstfit">First Fit</option>
-            <option value="CBIP">CBIP</option>
-          </select>
+            Value of k:
+            <select
+              value={valueOfK}
+              onChange={(event) => setValueOfK(event.target.value)}
+              style={{ marginLeft: '8px' }}
+            >
+              <option value="2" defaultValue>2</option>
+              <option value="4">4</option>
+            </select>
           </label>
+
+            <label style={{ marginBottom: '10px' }}>
+            Algorithm:
+            <select value={algorithm} onChange={(event) => setAlgorithm(event.target.value)} style={{ marginLeft: '15px' }}>
+              <option value="firstfit">First Fit</option>
+              <option value="CBIP">CBIP</option>
+            </select>
+            </label>
           
         </div>
         <button type="submit">Simulate Graph</button>
       </form>
 
-      {showGraph && algorithm === "firstfit" && <FirstFit numVertices={numVertices} valueOfK={valueOfK} />}
-      {showGraph && algorithm === "CBIP" && <CBIP numVertices={numVertices} valueOfK={valueOfK} />}
-
+      {showGraph && algorithm === "firstfit" && <FirstFit numVertices={numVertices} key={key} valueOfK={valueOfK} />}
+      {showGraph && algorithm === "CBIP" && <CBIP numVertices={numVertices} key={key} valueOfK={valueOfK} />}
+      
     </div>
   );
 };
